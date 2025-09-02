7d2ee16881a3114b68ad691ff0a59a174e6bfe5b
gojosaturo25 <vishalkumar3092874@gmail.com>
2025-09-02 21:50:19 +0530

    new thing

--- routes/review.js
+++ routes/review.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapasync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const Review = require("../models/review.js");
+
+// const Review = require("../models/review.js");
 const {
   validateReview,
   isLoggedIn,
   isReviewAuthor,
 } = require("../middleware.js");
-const Listing = require("../models/listing.js");
+// const Listing = require("../models/listing.js");
 const reviewController=require("../controllers/reviews.js")
 
 //reviews
