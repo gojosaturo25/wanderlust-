dd548c69760118515a2b207ff17bb05e21d1158c
gojosaturo25 <vishalkumar3092874@gmail.com>
2025-09-02 19:50:22 +0530

    add project files

---
+++ controllers/reviews.js
@@ -0,0 +1,29 @@
+const Listing = require("../models/listing");
+const Review = require("../models/review");
+
+module.exports.createReview = async (req, res) => {
+  let listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    req.flash("error", "Listing not found!");
+    return res.redirect("/listings");
+  }
+  let newReview = new Review(req.body.review);
+  newReview.author = req.user._id;
+  listing.reviews.push(newReview);
+  await newReview.save();
+  await listing.save();
+  req.flash("success", "New review created");
+  // res.send("review saved")
+
+  res.redirect(`/listings/${listing._id}`);
+};
+
+
+module.exports.destroyReview=async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "New review deleted");
+
+    res.redirect(`/listings/${id}`);
+  }
---
+++ routes/review.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapasync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Review = require("../models/review.js");
+const {
+  validateReview,
+  isLoggedIn,
+  isReviewAuthor,
+} = require("../middleware.js");
+const Listing = require("../models/listing.js");
+const reviewController=require("../controllers/reviews.js")
+
+//reviews
+//Post review route
+
+router.post(
+  "/",
+  validateReview,
+  isLoggedIn,
+  wrapAsync(reviewController.creteReview)
+);
+
+
+//Delete review route
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
+
+  wrapAsync(reviewController.destroyReview)
+);
+
+module.exports = router;
